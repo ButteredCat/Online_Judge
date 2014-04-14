c2baf6d59f6992c13365e872d85c5381723e0efc
ButteredCat <butteredcat@live.com>
2014-04-14 15:50:33 +0800

    1550, Apr 14, 2014

---
+++ pat/basic/1021.cpp
@@ -0,0 +1,22 @@
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+int
+main (void)
+{
+	string s;
+
+	while (cin >> s)
+	{
+		int m[10] = { 0 };
+		for (int i = 0; i < (int) s.size (); ++i)
+			++m[s[i] - '0'];
+		for (int i = 0; i < 10; ++i)
+			if (m[i] != 0)
+				cout << i << ":" << m[i] << endl;
+	}
+
+	return 0;
+}
