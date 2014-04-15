8c24ea23054a8901e4fe0ec088f468d6b51b4fe3
ButteredCat <butteredcat@live.com>
2014-04-15 15:59:17 +0800

    1600, Apr 15, 2014

--- pat/basic/1008.cpp
+++ pat/basic/1008.cpp
@@ -2,20 +2,47 @@
 
 using namespace std;
 
-int reverse(int *a, int beg, int end)
+void
+reverse (int *p, int begin, int end)
 {
+    for (; begin < end; ++begin, --end)
+    {
+        int temp = p[end];
+        p[end] = p[begin];
+        p[begin] = temp;
+    }
 }
 
-int main()
+void
+shiftRight (int *p, int n, int m)
 {
-    int n,m;
-    while(cin>>n>>m)
-    {
-        int *a=new int[n];
-        int i;
-        for(i=0; i<n; ++i)
-            cin>>a[i];
+    m = m % n;
+    reverse (p, 0, n - m - 1);
+    reverse (p, n - m, n - 1);
+    reverse (p, 0, n - 1);
+}
 
+int
+main ()
+{
+    int n, m;                   // length=n, shift right m
+
+    while (cin >> n >> m)
+    {
+        int i, *p = new int[n];
+
+        for (i = 0; i != n; ++i)
+            cin >> p[i];
+
+        shiftRight (p, n, m);
+
+        for (i = 0; i != n - 1; ++i)
+            cout << p[i] << " ";
+
+        cout << p[i] << endl;
+
+        delete[] p;
     }
+
     return 0;
 }
