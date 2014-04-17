30cdda74d62724aba73751496ff123358e0e8a88
ButteredCat <butteredcat@live.com>
2014-04-17 15:46:49 +0800

    1546, Apr 17, 2014

--- pat/basic/1016.c
+++ pat/basic/1016.c
@@ -1,37 +1,31 @@
 #include<stdio.h>
 
 int
+partial (char *number, char d)
+{
+	int part, weight = 1;
+
+	char *p = number;
+
+	part = 0;
+	while (*p != '\0')
+	{
+		if (*p++ == d)
+		{
+			part += (d - '0') * weight;
+			weight *= 10;
+		}
+	}
+	return part;
+}
+
+int
 main (void)
 {
-	char a[10], b[10], da, db, *p;
-
-	int pa, pb, weight;
+	char a[10], b[10], da, db;
 
 	while (scanf ("%s %c %s %c", a, &da, b, &db) != EOF)
-	{
-		p = a;
-		pa = 0;
-		weight = 1;
-		while (*p != '\0')
-		{
-			if (*p++ == da)
-			{
-				pa += (da - '0') * weight;
-				weight *= 10;
-			}
-		}
-		p = b;
-		pb = 0;
-		weight = 1;
-		while (*p != '\0')
-		{
-			if (*p++ == db)
-			{
-				pa += (db - '0') * weight;
-				weight *= 10;
-			}
-		}
-		printf ("%d\n", pa + pb);
-	}
+		printf ("%d\n", partial (a, da) + partial (b, db));
+
 	return 0;
 }
