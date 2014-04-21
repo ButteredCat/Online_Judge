395729b4322c0dae28963f87f5fdaefc7c13cb93
ButteredCat <butteredcat@live.com>
2014-04-21 22:21:03 +0800

    backup

--- pat/basic/1025.c
+++ pat/basic/1025.c
@@ -24,29 +24,48 @@ swap (int *pa, int *pb)
 void
 reverse (int n, int k)
 {
-	int i, m, last_tail, addr = head;
+	int i, m, last_tail, current_tail, addr = head;
 
 	m = n - n % k;
-	last_tail = head;
-	for (i = 0; i < m; ++i)
+	current_tail = head;
+	last_tail = -1;
+	for (i = 0; i < m - 1; ++i)
 	{
 		swap (&list[addr].next, &list[addr].prior);
-		printf ("addr=%d\n", addr);
+		//printf ("addr=%d\n", addr);
 		if (i % k == k - 1)
 		{
 			if (i == k - 1)
+			{
 				head = addr;
+			}
 			else
+			{
 				list[last_tail].next = addr;
+			}
 		}
-		if (i % k == 0)
+		if (i % k == 0 && i / k > 0)
 		{
-			last_tail = addr;
+			last_tail = current_tail;
+			current_tail = addr;
 		}
 		addr = list[addr].prior;
+		//printf ("current=%d last=%d\n", current_tail, last_tail);
 	}
-	printf ("addr=%d\n", addr);
-	list[last_tail].next = addr;
+	//printf ("addr=%d\n", addr);
+	if (list[addr].next != -1)
+	{
+		list[last_tail].next = addr;
+		list[current_tail].next = list[addr].next;
+	}
+	else
+	{
+		list[last_tail].next = addr;
+		list[current_tail].next = -1;
+	}
+	swap (&list[addr].next, &list[addr].prior);
+	if(i/k==0)
+		head=addr;
 }
 
 int
