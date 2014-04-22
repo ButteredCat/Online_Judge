38b9f48dc009900a96bab8123c81b24b10fd738e
ButteredCat <butteredcat@live.com>
2014-04-22 15:02:30 +0800

    1502, Apr 22, 2014

---
+++ leetcode/plus-one.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+        int carry=0;
+        vector<int>::reverse_iterator iv = digits.rbegin();
+        if(*iv+1>9) {
+            *iv=0;
+            carry=1;
+        }
+        else {
+            *iv+=1;
+        }
+        for(++iv; iv != digits.rend(); ++iv) {
+            if(*iv+carry>9) {
+                *iv=0;
+                carry=1;
+            }
+            else {
+                *iv+=carry;
+                carry=0;
+            }
+        }
+        if(carry==1) {
+            digits.insert(digits.begin(),carry);
+        }
+        
+        return digits;
+    }
+};
