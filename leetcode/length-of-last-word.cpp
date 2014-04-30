b1c0c273bf4358ad97cf2223fd187d3163fd7868
ButteredCat <butteredcat@live.com>
2014-04-30 21:10:21 +0800

    2110, Apr 30, 2014

--- leetcode/length-of-last-word.cpp
+++ leetcode/length-of-last-word.cpp
@@ -1,31 +1,33 @@
 /***************************************************************************
-Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+Given a string s consists of upper/lower-case alphabets and empty space
+characters ' ', return the length of last word in the string.
 
 If the last word does not exist, return 0.
 
-Note: A word is defined as a character sequence consists of non-space characters only.
+Note: A word is defined as a character sequence consists of non-space
+characters only.
 
 For example,
 Given s = "Hello World",
 return 5.
 ***************************************************************************/
+
 class Solution {
 public:
-    int searchInsert (int A[], int n, int target) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return A[0] >= target ? 0 : 1;
-        } else {
-            int middle = n / 2;
+    int lengthOfLastWord (const char *s) {
+        int i = strlen (s) - 1;
 
-            if (A[middle] == target) {
-                return middle;
-            } else if (A[middle] > target) {
-                return searchInsert (A, middle, target);
-            } else {
-                return middle + searchInsert (A + middle, n - middle, target);
-            }
+        while (i > 0 && s[i] == ' ') {
+            --i;
         }
+
+        int length = 0;
+
+        while (i >= 0 && s[i] != ' ' ) {
+            ++length;
+            --i;
+        }
+
+        return length;
     }
 };
