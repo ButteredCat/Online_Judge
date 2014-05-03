7b17c0a09cd93430a67a9c274c41cbf8181dd9a5
ButteredCat <butteredcat@live.com>
2014-05-03 12:45:43 +0800

    1245, May 3, 2014

--- leetcode/best-time-to-buy-and-sell-stock-ii.cpp
+++ leetcode/best-time-to-buy-and-sell-stock-ii.cpp
@@ -24,3 +24,36 @@ public:
         return max_profit;
     }
 };
+
+class Solution_2 {
+public:
+    // A simulation for the real trade. If you sell in day i, you mustn't buy until day i+1
+    int maxProfit (vector<int> &prices) {
+        int max_profit = 0;
+
+        if (prices.size() > 1) {
+            int lowest = prices[0];
+            bool onTrans = (prices[0] < prices[1]);
+            vector<int>::size_type i = 1;
+
+            for ( ; i != prices.size() - 1; ++i) {
+                // Find local maximum. If found, sell.
+                if (onTrans && prices[i] >= prices[i - 1] && prices[i] >= prices[i + 1]) {
+                    max_profit += prices[i] - lowest;
+                    onTrans = false;
+                }
+                // Find local minimum. If found, buy.
+                else if (prices[i] <= prices[i - 1] && prices[i] <= prices[i - 1] ) {
+                    lowest = prices[i];
+                    onTrans = true;
+                }
+            }
+
+            if (onTrans && prices[i] >= prices[i - 1]) {
+                max_profit += prices[i] - lowest;
+            }
+        }
+
+        return max_profit;
+    }
+};
