ec5bdb4dc928b0504f2f9051fa7649999b1c27a5
ButteredCat <butteredcat@live.com>
2014-05-04 16:01:34 +0800

    1601, May 4, 2014

---
+++ leetcode/evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,50 @@
+/**
+ * Evaluate the value of an arithmetic expression in Reverse Polish
+ * Notation.
+ *
+ * Valid operators are +, -, *, /. Each operand may be an integer or another
+ * expression.
+ *
+ * Some examples:
+ *   ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+ *   ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+ */
+
+class Solution {
+public:
+    int evalRPN (vector<string> &tokens) {
+        stack<int> opnd;
+
+        for (vector<string>::iterator iv = tokens.begin(); iv != tokens.end(); ++iv ) {
+            int a, b;
+
+            if (*iv == "+" || *iv == "-" || *iv == "*" || *iv == "/") {
+                int a = opnd.top();
+                opnd.pop();
+                int b = opnd.top();
+                opnd.pop();
+
+                switch ( (*iv) [0] ) {
+                case '+':
+                    opnd.push (a + b);
+                    break;
+                case '-':
+                    opnd.push (b - a);
+                    break;
+                case '*':
+                    opnd.push (a * b);
+                    break;
+                case '/':
+                    opnd.push (b / a);
+                    break;
+                default:
+                    break;
+                }
+            } else {
+                opnd.push (stoi (*iv) );
+            }
+        }
+
+        return opnd.top();
+    }
+};
